eeef1f33db77fc952372f6a5c7f5fde44bb290a7
yifanr <yifan_ruan@brown.edu>
2021-12-16 00:49:31 -0500

    tweaks to border points

--- polygonize.py
+++ polygonize.py
@@ -2,10 +2,14 @@ from typing import Tuple
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import Delaunay
-from skimage import filters, img_as_float32, io, draw
+from skimage import filters, img_as_float32, io, draw, img_as_ubyte
 from skimage.color import rgb2gray
 from sklearn.cluster import MiniBatchKMeans
+import os
 
+
+def save_image(path, im):
+    return io.imsave(path, img_as_ubyte(im.copy()))
 
 def cluster_img(img: np.ndarray, clusters: int) -> np.ndarray:
     """Form clusters from an input image"""
@@ -89,18 +93,28 @@ def triangulate(original_img: np.ndarray, clustered_img: np.ndarray) -> Tuple[np
     # Add image border points
     print(j)
 
-    STEP = 50
+
     height = original_img.shape[0]  # j
     width = original_img.shape[1]  # i
-
-    top_border_points_j = np.zeros(width // STEP + 1)
-    top_border_points_i = np.arange(0, width-1, STEP)
-    bottom_border_points_j = np.full(width // STEP + 1, height-1)
-    bottom_border_points_i = np.arange(0, width-1, STEP)
-    left_border_points_j = np.arange(0, height-1, STEP)
-    left_border_points_i = np.zeros(height // STEP + 1)
-    right_border_points_j = np.arange(0, height-1, STEP)
-    right_border_points_i = np.full(height // STEP + 1, width-1)
+    
+    numDiv = 40
+    top_border_points_i = []
+    bottom_border_points_i = []
+    left_border_points_j = []
+    right_border_points_j = []
+    for k in range(numDiv+1):
+        top_border_points_i.append((int)((k/numDiv)*(width-1)))
+        bottom_border_points_i.append((int)((k/numDiv)*(width-1)))
+        left_border_points_j.append((int)((k/numDiv)*(height-1)))
+        right_border_points_j.append((int)((k/numDiv)*(height-1)))
+    top_border_points_i = np.array(top_border_points_i)
+    bottom_border_points_i = np.array(bottom_border_points_i)
+    left_border_points_j = np.array(left_border_points_j)
+    right_border_points_j = np.array(right_border_points_j)
+    top_border_points_j = np.zeros(numDiv+1)
+    bottom_border_points_j = np.full(numDiv+1, height-1)
+    left_border_points_i = np.zeros(numDiv+1)
+    right_border_points_i = np.full(numDiv+1, width-1)
 
     j = np.concatenate(
         [
@@ -112,6 +126,7 @@ def triangulate(original_img: np.ndarray, clustered_img: np.ndarray) -> Tuple[np
         ],
         axis=0
     )
+    print(j.shape)
 
     i = np.concatenate(
         [
@@ -123,6 +138,7 @@ def triangulate(original_img: np.ndarray, clustered_img: np.ndarray) -> Tuple[np
         ],
         axis=0
     )
+    print(i.shape)
 
     points = np.concatenate([i[:, None], j[:, None]], axis=1)
 
@@ -148,6 +164,7 @@ def visualize(img: np.ndarray, points: np.ndarray, simplices: np.ndarray) -> Non
         img[draw.polygon(triangle[:,1], triangle[:,0])] = color
     plt.imshow(img)
     plt.show()
+    return img
 
 def polygonize(path: str, clusters: int) -> None:
     """Polygonize a specified image with a specified number of clusters for segmentation."""
@@ -159,4 +176,5 @@ def polygonize(path: str, clusters: int) -> None:
     points, simplices = triangulate(img, res)
 
     # Visualize
-    visualize(img, points, simplices)
+    result = visualize(img, points, simplices)
+    save_image("results" + os.sep + str.rsplit(path, sep=os.sep, maxsplit=1)[1], result)
